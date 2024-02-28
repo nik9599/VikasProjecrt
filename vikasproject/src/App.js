e86dd7dcf62366240ff7bdca85e1a2b2016b5053
Nikhil <nk9599094257@gmail.com>
2024-02-28 22:07:32 +0530

    web view done

--- vikasproject/src/App.js
+++ vikasproject/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Home from './Component/Pages/LandingPage/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+       <Home/>
     </div>
   );
 }
